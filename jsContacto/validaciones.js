d4e6c5092edda106e8bd550a525b132ffdf63968
Adrian Alexis Bravo Armendariz <aabravo6@espe.edu.ec>
2022-11-08 13:19:39 -0500

    Solucion_CODIGO

---
+++ jsContacto/validaciones.js
@@ -0,0 +1,57 @@
+const form = document.getElementById("FormContacto");
+const expresion = {
+	email:  /^[a-z][\w.-]+@\w[\w.-]+\.[\w.-]*[a-z][a-z]$/i, // Correo Electronico.
+	Telef: /^\d{10}$/
+	
+}
+
+function emailValido() {
+	var valor = document.getElementById("email").value;
+	var vlce = false;
+	if (!expresion.email.test(valor)){
+		alert('El correo eletrónico no tiene el formato correcto');
+		vlce = false;
+	}
+	else{
+		console.log("Gracias por ingresar su correo electrónico");
+		vlce = true;
+	}
+	return vlce;
+}
+
+function telefonoValido(){
+	var valor = document.getElementById("telefono").value;
+	var vltl = false;
+	if(valor.length == 10 && !isNaN(valor)){
+		vltl = true;
+	}else{
+		alert('El telefono ingresado no tiene el formato correcto');
+	}
+	return vltl;
+}
+
+function delay(n){
+    return new Promise(function(resolve){
+        setTimeout(resolve,n*1000);
+    });
+}
+
+function validar(){
+	if(emailValido() && telefonoValido()){
+		alert("Los datos no tienen el formato correcto");
+		return true;
+	}else{
+		return false;
+	}
+}
+
+async function deshabilitar(){
+	if(validar() == true){
+		var btn = document.getElementById('btn');
+		btn.disabled = true;
+		btn.value = 'Enviando datos';
+		await delay(2);
+		btn.form.submit();
+	}
+
+}
